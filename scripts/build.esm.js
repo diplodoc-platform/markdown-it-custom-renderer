9d46297cf572ae7e4d83bc5ca47c367823ee3801
moki <morozov.kirill.moki@gmail.com>
2023-04-10 00:06:45 +0400

    chore: project initial infrastructure

---
+++ scripts/build.esm.js
@@ -0,0 +1,26 @@
+const esbuild = require('esbuild');
+const {nodeExternalsPlugin} = require('esbuild-node-externals');
+const {Generator} = require('npm-dts');
+
+const {dependencies = [], peerDependencies = []} = require('../package.json');
+
+esbuild
+    .build({
+        entryPoints: ['src/index.ts'],
+        outfile: 'lib/esm/index.js',
+        bundle: true,
+        format: 'esm',
+        minify: true,
+        platform: 'node',
+        sourcemap: true,
+        target: 'node14',
+        external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
+        plugins: [nodeExternalsPlugin()],
+    })
+    .then(() => {
+        new Generator({
+            entry: 'src/index.ts',
+            output: 'lib/index.d.ts',
+        }).generate();
+    })
+    .catch(() => process.exit(1));
